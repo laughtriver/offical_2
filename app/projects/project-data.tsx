fad31f1fb4fe8fbf2940a68c157071499ab2733a
KTaisei <mathlike.science@gmail.com>
2024-10-31 22:02:49 +0900

    最終調整

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -7,24 +7,17 @@ export interface Project {
 
 export const projects: Project[] = [
   {
-    title: "Project One",
-    year: 2023,
+    title: "Our birthday",
+    year: 2024,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "「今日誕生日の人おめでとう！」を形にしたSNS!みんなで誰か知らない人の誕生日を祝うSNSです!",
+    url: "https://birthday-dp1.pages.dev/",
   },
   {
-    title: "Project Two",
-    year: 2022,
+    title: "パスワード生成",
+    year: 2024,
     description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
-  },
-  {
-    title: "Project Three",
-    year: 2021,
-    description:
-      "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laboriosam veritatis dolorem rem praesentium dicta labore, at laudantium quisquam.",
-    url: "https://example.com/",
+      "このプログラムは、pythonによる、パスワード生成プログラムです。こちらをクリックすることでダウンロードが可能です。※pythonがインストールされている環境で実行して下さい",
+    url: "https://drive.google.com/file/d/1QsCeLgVbO97ZBsw93eN4jwKyE08obHbX/view?usp=drive_link",
   },
 ];
